074f09128e9016f5642b457b81133e6d538c0671
cheap95 <cheap95@gmail.com>
2021-03-22 21:33:52 +0900

    新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
     
     private
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(sign_up, keys: [:nickname, :first_family_name, :last_family_name, :first_family_name_kana, :last_family_name_kana, :birthday])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_family_name, :last_family_name, :first_family_name_kana, :last_family_name_kana, :birthday])
+        #devise_parameter_sanitizer.permit(:sign_in, keys: 
     end
 
 end
