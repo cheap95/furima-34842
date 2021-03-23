a959936bb03b92adebf0f7f9959a9648ab070ecf
cheap95 <cheap95@gmail.com>
2021-03-23 14:52:53 +0900

    管理機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
     
+
     private
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_family_name, :last_family_name, :first_family_name_kana, :last_family_name_kana, :birthday])
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,8 @@ class ItemsController < ApplicationController
   def update
   end
   def destroy
+    log_out
+    redirect_to root_path
   end
 
   private
