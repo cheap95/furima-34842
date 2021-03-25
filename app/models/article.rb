63e795963efc32613c910208fec084fcf78597fe
cheap95 <cheap95@gmail.com>
2021-03-25 21:36:56 +0900

    Active Hash 3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,13 @@
 class Article < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+    belongs_to_active_hash :category, :area, :delivery_day, :delivery_fee, :condition
+    belongs_to :item
   
   validates :title, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+  
+  validates :area_id, numericality: { other_than: 0 } 
+  validates :category_id, numericality: { other_than: 0 } 
+  validates :delivery_day_id, numericality: { other_than: 0 } 
+  validates :delivery_fee_id, numericality: { other_than: 0 } 
+  validates :condition_id, numericality: { other_than: 0 } 
 end
