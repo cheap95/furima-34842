aa2e4bf8a71cbbdf31c9b81eeb5f3083c382692c
cheap95 <cheap95@gmail.com>
2021-03-25 17:07:32 +0900

    ビューの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,14 @@
 class ItemsController < ApplicationController
-    
+  def index
+    @item = Item.all
+  end
+
+  def create
+    if @item = Item.create(item_params)
+       @item.save
+       redirect_to root_path
+    #else
+       #render :new
+    end
+  end
 end
