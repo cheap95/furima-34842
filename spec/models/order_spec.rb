fce8851da20ce86ae93d4d21a8383015f0a3e815
cheap95 <cheap95@gmail.com>
2021-04-05 16:17:50 +0900

    テストコード途中

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,106 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+  describe "ユーザー新規登録" do
+    context "新規登録できるとき" do
+      it "" do
+        expect(@order).to be_valid
+       end
+       it "" do
+        expect(@order).to be_valid
+       end
+       it "" do
+        expect(@order).to be_valid
+       end
+       it "" do
+        expect(@order).to be_valid
+       end
+       it "" do
+        expect(@order).to be_valid
+       end
+       it "" do
+        expect(@order).to be_valid
+       end     
+    end
+    context "新規登録できないとき" do
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it "" do
+        @order.nickname = ""
+        @order.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      
+  end
 end
