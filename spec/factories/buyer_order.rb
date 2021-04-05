f095d131c22e51960ed4bb34fd9a0bf4d7298d5d
cheap95 <cheap95@gmail.com>
2021-04-05 19:18:50 +0900

    購入機能実装完了

---
+++ spec/factories/buyer_order.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+    factory :buyer_order do
+      city {"あ"}
+      address {1-1-1}
+      post_code {"123-4567"}
+      phone_number {"09012345678"}
+      area_id {4}
+      token {"aaaaaaaaaaaaa"}
+      item_id {3}
+      user_id {3}
+    end
+  end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,9 +1,3 @@
 FactoryBot.define do
-  factory :order do
-    city {"あ"}
-    address {1-1-1}
-    post_code {123-4567}
-    phone_number {09012345678}
-    area_id {4}
-  end
+  
 end
